953ec084d36a6f7bae174f169560ce9450db6054
yazz <133223919+yaznzamel@users.noreply.github.com>
2026-02-28 19:49:16 +0300

    implement token based rate limiting

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import threading
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from socketserver import ThreadingMixIn
+import time
+import tracemalloc 
+
+
+TOKENS = 5
+
 
 # Define the HTTP request handler class
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 # Override the do_GET method to handle GET requests
     def do_GET(self):
+        global TOKENS
         # Set the response status code
-        self.send_response(200)
-        # Set the response headers
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        # Write the response content
-        self.wfile.write(b"Hello, world!")
+        if TOKENS > 0 :
+            TOKENS -=1
+            self.send_response(200)
+            # Set the response headers
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            # Write the response content
+            self.wfile.write(b"Hello, world!")
+            time.sleep(3)
+
+        else:
+            self.send_response(429)
+            self.end_headers()
+            self.wfile.write(b"Too many requests - no tokens left")
 
 # Define a threaded HTTP server using ThreadingMixIn
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
@@ -21,6 +36,7 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
 # Main function to run the server
 def main():
 # Define the server address and port
+    tracemalloc.start()
     server_address = ('', 8000)
     # Create an instance of the threaded HTTP server
     httpd = ThreadedHTTPServer(server_address, SimpleHTTPRequestHandler)
@@ -34,6 +50,13 @@ def main():
         # Shutdown the server gracefully
         httpd.server_close()
         print("Server stopped.")
+        # --- NEW MEMORY PROFILING LOGIC ---
+        print("[ System Memory Snapshot ]")
+        snapshot = tracemalloc.take_snapshot()
+        top_stats = snapshot.statistics('lineno')
+        
+        for stat in top_stats[:10]: # Print the top 10 memory consumers
+            print(stat)
 
 
 
